bcac6dfcdb89ffc574dc44b3747ddbfe5a2880b2
Eliad Peller <eliad@wizery.com>
2012-03-11 15:47:00 +0200

    mmc: omap_hsmmc: set pm_runtime_irq_safe
    Declare the pm runtime handlers as safe to run
    in atomic context (it's in fact being called
    from mmc_signal_sdio_irq() in irq context)
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -2475,6 +2475,7 @@ static int __init omap_hsmmc_probe(struct platform_device *pdev)
 	host->dpm_state = DISABLED;
 
 	pm_runtime_enable(host->dev);
+	pm_runtime_irq_safe(host->dev);
 #ifndef CONFIG_PM_RUNTIME
 	/*
 	 * If runtime PM is not enabled, ensure clocks are always enabled.
